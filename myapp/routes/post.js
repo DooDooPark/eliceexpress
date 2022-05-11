e0363e1ae7571bb117155f0439c78e65eb7318b9
박두현 <doubled4017@gmail.com>
2022-05-11 15:05:01 +0900

    마지막포스트 5-11 수업

--- myapp/routes/post.js
+++ myapp/routes/post.js
@@ -6,12 +6,58 @@ router.get('/', (req, res) => {
     res.render('post');
 });
 
+router.get('/del', (req, res) => {
+    res.render('delete')
+})
+
+router.get('/bookinfo/:id', (req, res) => {
+    const authorname = req.params.id;
+
+    // Movie.find({ year: { $gte: 1980, $lte: 1989 } }, function(err, arr) {});
+    // BookSchema.findOne({ auther: authorname }, (err, result) => {
+    //     if (result) {
+    //         return res.json(result);
+    //     } else {
+    //         return res.send('등록된 작가가 없습니다');
+    //     }
+    // });
+    BookSchema.find({ auther: authorname })
+        .then(result => {
+            res.json(result);
+        }).catch(err => {
+            console.log(err);
+        });
+});
+
+router.delete('/del/:id', (req, res) => {
+    const bookname = req.params.id;
+
+    BookSchema.findOneAndDelete({ bookname: bookname })
+        .then(result => {
+            res.json({ redirect: '/expost' });
+        }).catch(err => {
+            console.log(err);
+        })
+});
+
+router.post('/del/:id', (req, res) => {
+    const bookname = req.params.id;
+
+    BookSchema.findOneAndDelete({ bookname: bookname })
+        .then(result => {
+            res.json({ redirect: '/expost' });
+        }).catch(err => {
+            console.log(err);
+        })
+});
+
+//get post, put==> (업데이트, 수정), delete
+
 router.post('/', (req, res) => {
     const name = req.body.name;
     const number = req.body.phone;
     const date = req.body.date;
     //요청.
-
     //응답.
     res.json({ name: name, number: number, date: date });
 });
