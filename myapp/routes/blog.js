7a67594eb1f5d412e546d4ec1e01e095cd8f6fc8
박두현 <doubled4017@gmail.com>
2022-05-16 13:54:32 +0900

    5/16 3교시

--- myapp/routes/blog.js
+++ myapp/routes/blog.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+//localhost:3000/blog
+
 router.get('/', (req, res) => {
     res.render('blog/blog');
 });
+
+router.get('/write', (req, res) => {
+    res.render('blog/write');
+})
 
 //localhost:3000/blog
 
