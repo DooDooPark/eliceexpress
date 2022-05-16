de0cda32c2ec74c3ba279e6671f85b8d45bc1c31
박두현 <doubled4017@gmail.com>
2022-05-16 14:30:01 +0900

    4시간

---
+++ myapp/models/blog.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const blogSchema = mongoose.Schema;
+
+// 블로그 글 제목.
+// 블로그 본 글 내용.
+
+const blog = new blogSchema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const blogModel = mongoose.model('blog', blog);
+module.exports = blogModel;
--- myapp/routes/blog.js
+++ myapp/routes/blog.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const blogSchema = require('../models/blog');
 const router = express.Router();
 
 //localhost:3000/blog
@@ -9,7 +10,25 @@ router.get('/', (req, res) => {
 
 router.get('/write', (req, res) => {
     res.render('blog/write');
-})
+});
+
+router.post('/write', (req, res, next) => {
+    const title = req.body.title;
+    const content = req.body.content;
+
+    const blogText = new blogSchema({
+        title: title,
+        content: content,
+    });
+
+    blogText.save().then(result => {
+        console.log(result);
+        res.redirect('/blog');
+    }).catch(err => {
+        console.log(err);
+        next(err);
+    })
+});
 
 //localhost:3000/blog
 
