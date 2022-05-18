89877341f3ba4fec0a3e4bf404b01260bac643e8
박두현 <doubled4017@gmail.com>
2022-05-18 10:50:25 +0900

    마지막 1시간 강의

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,10 +1,27 @@
 var express = require('express');
 const userSchema = require('../models/newuser');
+const bcrypt = require('bcrypt');
+const { body, validationResult } = require('express-validator');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.render('blog/auth');
 });
+
+//정규 표현식. 
+//passport
+
+
+router.post('/signup', body('email').isEmail(),
+  body('password').isLength({ min: 5 })
+  , (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array()
+      })
+    }
+  });
 
 module.exports = router;
